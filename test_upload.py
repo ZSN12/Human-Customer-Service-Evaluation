3c2fa3f87e50dce7d9955785af898a6025aa3dfa
张少楠 <3544402477@qq.com>
2026-04-25 23:14:40 +0800

    Delete test_upload.py

--- test_upload.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost:8001/api/upload'
-files = {'file': open('test.csv', 'rb')}
-
-response = requests.post(url, files=files)
-print('Status code:', response.status_code)
-print('Response:', response.text)
